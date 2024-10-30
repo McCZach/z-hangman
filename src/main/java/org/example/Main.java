b67288e41a3bf0cfd1608654402358a4053977f1
Zach McColgan <99850300+McCZach@users.noreply.github.com>
2024-10-30 14:25:42 -0500

    add game functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -27,11 +28,93 @@ public class Main {
 
         return word;
     }
+
+    public static char getInput(Scanner scan)
+    {
+        char input = ' ';
+
+        do {
+            try
+            {
+                System.out.print("Guess: ");
+                input = scan.next().charAt(0);
+                System.out.println();
+            }
+            catch (Exception ex)
+            {
+                System.out.println("Error reading in guess!");
+                System.out.println(ex.getMessage());
+            }
+        } while (!Character.isLetter(input));
+
+        return Character.toLowerCase(input);
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         String word = getWord();
 
+        char[] guess = new char[word.length()];
+        Arrays.fill(guess, '_');
+        char letter = ' ';
+
+        int lives = word.length() / 2;
+        boolean gameOver, guessSuccess, lost;
+        lost = gameOver = guessSuccess = false;
+
+
+        System.out.println("\n\n***GUESS THAT WORD***\n");
+        for (int i = 0; i < word.length(); i++)
+        {
+            System.out.print(guess[i] + " ");
+        }
+        System.out.print("\t-> Lives = " + lives + "\n");
+
+        while ((!lost) && (!gameOver))
+        {
+            letter = getInput(scan);
+
+            for (int i = 0; i < word.length(); i++)
+            {
+                if (word.charAt(i) == letter)
+                {
+                    guess[i] = letter;
+                    guessSuccess = true;
+                }
+            }
+
+            if (!guessSuccess)
+                lives--;
+            guessSuccess = false;
+
+            if (lives == 0)
+                lost = true;
+
+            for (int i = 0; i < word.length(); i++)
+            {
+                System.out.print(guess[i] + " ");
+            }
+            System.out.print("\t-> Lives = " + lives + "\n");
+
+            if (Arrays.toString(guess).equals(word))
+            {
+                gameOver = true;
+            }
+        }
+
+        if (lost)
+        {
+            System.out.println("\nYou lost - You ran out of guesses! The word was: " + word);
+            for (int i = 0; i < word.length(); i++)
+            {
+                System.out.print(guess[i] + " ");
+            }
+        }
+        else
+        {
+            System.out.println("You won - With: " + lives + "lives left! The word was: " + word);
+        }
 
 
     }
